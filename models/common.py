9388add146ab2f304af388202aa5a38e1e89dfd1
Bhimendra Dewangan <bhimendradewangan97@gmail.com>
2024-04-12 22:26:25 +0530

    full new fusion

--- models/common.py
+++ models/common.py
@@ -30,67 +30,376 @@ from utils.general import (LOGGER, ROOT, Profile, check_requirements, check_suff
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import copy_attr, smart_inference_mode
 
-class SelfAttention(nn.Module):
-    def __init__(self, in_channels, reduction_ratio=8):
-        super(SelfAttention, self).__init__()
+# YOLOv5 common modules
 
-        self.query_conv = nn.Conv2d(in_channels, in_channels // reduction_ratio, kernel_size=1)
-        self.key_conv = nn.Conv2d(in_channels, in_channels // reduction_ratio, kernel_size=1)
-        self.value_conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
-        self.gamma = nn.Parameter(torch.zeros(1))  # Initialize gamma with zero
+import torch.nn.functional as F
+
+from utils.datasets import letterbox
+from utils.general import non_max_suppression, make_divisible, scale_coords, increment_path, xyxy2xywh, save_one_box
+from utils.plots import colors, plot_one_box
+from utils.torch_utils import time_synchronized
+
+from torch.nn import init, Sequential
+
+
+def autopad(k, p=None):  # kernel, padding
+    # Pad to 'same'
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+def DWConv(c1, c2, k=1, s=1, act=True):
+    # Depthwise convolution
+    return Conv(c1, c2, k, s, g=math.gcd(c1, c2), act=act)
+
+
+class Convv(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super(Conv, self).__init__()
+        # print(c1, c2, k, s,)
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
 
     def forward(self, x):
-        batch_size, channels, height, width = x.size()
+        # print("Conv", x.shape)
+        return self.act(self.bn(self.conv(x)))
 
-        # Project input tensors to query, key, and value
-        proj_query = self.query_conv(x).view(batch_size, -1, height * width).permute(0, 2, 1)  # B x (N) x C
-        proj_key = self.key_conv(x).view(batch_size, -1, height * width)  # B x C x (N)
-        energy = torch.bmm(proj_query, proj_key)  # Batch matrix multiplication
-        attention = F.softmax(energy, dim=-1)  # Attention map
-        proj_value = self.value_conv(x).view(batch_size, -1, height * width)  # B x C x (N)
+    def fuseforward(self, x):
+        return self.act(self.conv(x))
 
-        # Apply attention to value
-        out = torch.bmm(attention.permute(0, 2, 1), proj_value)
-        out = out.view(batch_size, channels, height, width)
 
-        # Apply gamma to the attended feature map and add it to the original input
-        out = self.gamma * out + x
+class TransformerLayer(nn.Module):
+    # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
+    def __init__(self, c, num_heads):
+        super().__init__()
+        self.q = nn.Linear(c, c, bias=False)
+        self.k = nn.Linear(c, c, bias=False)
+        self.v = nn.Linear(c, c, bias=False)
+        self.ma = nn.MultiheadAttention(embed_dim=c, num_heads=num_heads)
+        self.fc1 = nn.Linear(c, c, bias=False)
+        self.fc2 = nn.Linear(c, c, bias=False)
+
+    def forward(self, x):
+        x = self.ma(self.q(x), self.k(x), self.v(x))[0] + x
+        x = self.fc2(self.fc1(x)) + x
+        return x
+
+
+class TransformerBlock(nn.Module):
+    # Vision Transformer https://arxiv.org/abs/2010.11929
+    def __init__(self, c1, c2, num_heads, num_layers):
+        super().__init__()
+        self.conv = None
+        if c1 != c2:
+            self.conv = Conv(c1, c2)
+        self.linear = nn.Linear(c2, c2)  # learnable position embedding
+        self.tr = nn.Sequential(*[TransformerLayer(c2, num_heads) for _ in range(num_layers)])
+        self.c2 = c2
+
+    def forward(self, x):
+        if self.conv is not None:
+            x = self.conv(x)
+        b, _, w, h = x.shape
+        p = x.flatten(2)
+        p = p.unsqueeze(0)
+        p = p.transpose(0, 3)
+        p = p.squeeze(3)
+        e = self.linear(p)
+        x = p + e
+
+        x = self.tr(x)
+        x = x.unsqueeze(3)
+        x = x.transpose(0, 3)
+        x = x.reshape(b, self.c2, w, h)
+        return x
+
+
+
+class C33(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super(C3, self).__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # act=FReLU(c2)
+        self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)])
+        # self.m = nn.Sequential(*[CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)])
+
+    def forward(self, x):
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
+
+
+class Focus(nn.Module):
+    # Focus wh information into c-space
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super(Focus, self).__init__()
+        # print("c1 * 4, c2, k", c1 * 4, c2, k)
+        self.conv = Conv(c1 * 4, c2, k, s, p, g, act)
+        # self.contract = Contract(gain=2)
+
+    def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
+        # print("Focus inputs shape", x.shape)
+        # print()
+        return self.conv(torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1))
+        # return self.conv(self.contract(x))
+
+
+class Add(nn.Module):
+    #  Add two tensors
+    def __init__(self, arg):
+        super(Add, self).__init__()
+        self.arg = arg
+
+    def forward(self, x):
+        return torch.add(x[0], x[1])
+
+
+class Add2(nn.Module):
+    #  x + transformer[0] or x + transformer[1]
+    def __init__(self, c1, index):
+        super().__init__()
+        self.index = index
+
+    def forward(self, x):
+        if self.index == 0:
+            return torch.add(x[0], x[1][0])
+        elif self.index == 1:
+            return torch.add(x[0], x[1][1])
+        # return torch.add(x[0], x[1])
+
+
+
+class SPP(nn.Module):
+    # Spatial pyramid pooling layer used in YOLOv3-SPP
+    def __init__(self, c1, c2, k=(5, 9, 13)):
+        super(SPP, self).__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * (len(k) + 1), c2, 1, 1)
+        self.m = nn.ModuleList([nn.MaxPool2d(kernel_size=x, stride=1, padding=x // 2) for x in k])
+
+    def forward(self, x):
+        x = self.cv1(x)
+        return self.cv2(torch.cat([x] + [m(x) for m in self.m], 1))
+
+
+class SelfAttention(nn.Module):
+    """
+     Multi-head masked self-attention layer
+    """
+
+    def __init__(self, d_model, d_k, d_v, h, attn_pdrop=.1, resid_pdrop=.1):
+        '''
+        :param d_model: Output dimensionality of the model
+        :param d_k: Dimensionality of queries and keys
+        :param d_v: Dimensionality of values
+        :param h: Number of heads
+        '''
+        super(SelfAttention, self).__init__()
+        assert d_k % h == 0
+        self.d_model = d_model
+        self.d_k = d_model // h
+        self.d_v = d_model // h
+        self.h = h
+
+        # key, query, value projections for all heads
+        self.que_proj = nn.Linear(d_model, h * self.d_k)  # query projection
+        self.key_proj = nn.Linear(d_model, h * self.d_k)  # key projection
+        self.val_proj = nn.Linear(d_model, h * self.d_v)  # value projection
+        self.out_proj = nn.Linear(h * self.d_v, d_model)  # output projection
+
+        # regularization
+        self.attn_drop = nn.Dropout(attn_pdrop)
+        self.resid_drop = nn.Dropout(resid_pdrop)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, x, attention_mask=None, attention_weights=None):
+        '''
+        Computes Self-Attention
+        Args:
+            x (tensor): input (token) dim:(b_s, nx, c),
+                b_s means batch size
+                nx means length, for CNN, equals H*W, i.e. the length of feature maps
+                c means channel, i.e. the channel of feature maps
+            attention_mask: Mask over attention values (b_s, h, nq, nk). True indicates masking.
+            attention_weights: Multiplicative weights for attention values (b_s, h, nq, nk).
+        Return:
+            output (tensor): dim:(b_s, nx, c)
+        '''
+
+        b_s, nq = x.shape[:2]
+        nk = x.shape[1]
+        q = self.que_proj(x).view(b_s, nq, self.h, self.d_k).permute(0, 2, 1, 3)  # (b_s, h, nq, d_k)
+        k = self.key_proj(x).view(b_s, nk, self.h, self.d_k).permute(0, 2, 3, 1)  # (b_s, h, d_k, nk) K^T
+        v = self.val_proj(x).view(b_s, nk, self.h, self.d_v).permute(0, 2, 1, 3)  # (b_s, h, nk, d_v)
+
+        # Self-Attention
+        #  :math:`(\text(Attention(Q,K,V) = Softmax((Q*K^T)/\sqrt(d_k))`
+        att = torch.matmul(q, k) / np.sqrt(self.d_k)  # (b_s, h, nq, nk)
+
+        # weight and mask
+        if attention_weights is not None:
+            att = att * attention_weights
+        if attention_mask is not None:
+            att = att.masked_fill(attention_mask, -np.inf)
+
+        # get attention matrix
+        att = torch.softmax(att, -1)
+        att = self.attn_drop(att)
+
+        # output
+        out = torch.matmul(att, v).permute(0, 2, 1, 3).contiguous().view(b_s, nq, self.h * self.d_v)  # (b_s, nq, h*d_v)
+        out = self.resid_drop(self.out_proj(out))  # (b_s, nq, d_model)
 
         return out
 
 
-# import torch
-# import torch.nn as nn
-# import torch.nn.functional as F
+class myTransformerBlock(nn.Module):
+    """ Transformer block """
 
-# class SelfAttention(nn.Module):
-#     def __init__(self, in_channels, reduction_ratio=8):
-#         super(SelfAttention, self).__init__()
+    def __init__(self, d_model, d_k, d_v, h, block_exp, attn_pdrop, resid_pdrop):
+        """
+        :param d_model: Output dimensionality of the model
+        :param d_k: Dimensionality of queries and keys
+        :param d_v: Dimensionality of values
+        :param h: Number of heads
+        :param block_exp: Expansion factor for MLP (feed foreword network)
 
-#         self.query_conv = nn.Conv2d(in_channels, in_channels // reduction_ratio, kernel_size=1)
-#         self.key_conv = nn.Conv2d(in_channels, in_channels // reduction_ratio, kernel_size=1)
-#         self.value_conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
-#         self.gamma = nn.Parameter(torch.tensor(0.1))  # Initialize gamma with a small value
+        """
+        super().__init__()
+        self.ln_input = nn.LayerNorm(d_model)
+        self.ln_output = nn.LayerNorm(d_model)
+        self.sa = SelfAttention(d_model, d_k, d_v, h, attn_pdrop, resid_pdrop)
+        self.mlp = nn.Sequential(
+            nn.Linear(d_model, block_exp * d_model),
+            # nn.SiLU(),  # changed from GELU
+            nn.GELU(),  # changed from GELU
+            nn.Linear(block_exp * d_model, d_model),
+            nn.Dropout(resid_pdrop),
+        )
 
-#     def forward(self, x):
-#         batch_size, channels, height, width = x.size()
+    def forward(self, x):
+        bs, nx, c = x.size()
 
-#         # Project input tensors to query, key, and value
-#         proj_query = self.query_conv(x).view(batch_size, -1, height * width).permute(0, 2, 1)  # B x (N) x C
-#         proj_key = self.key_conv(x).view(batch_size, -1, height * width)  # B x C x (N)
-#         energy = torch.bmm(proj_query, proj_key)  # Batch matrix multiplication
-#         attention = F.softmax(energy, dim=-1)  # Attention map
-#         attention = attention / torch.sqrt(torch.tensor(channels).float())  # Normalize attention weights
-#         proj_value = self.value_conv(x).view(batch_size, -1, height * width)  # B x C x (N)
+        x = x + self.sa(self.ln_input(x))
+        x = x + self.mlp(self.ln_output(x))
 
-#         # Apply attention to value
-#         out = torch.bmm(attention.permute(0, 2, 1), proj_value)
-#         out = out.view(batch_size, channels, height, width)
+        return x
 
-#         # Apply gamma to the attended feature map and add it to the original input
-#         out = self.gamma * out + x
 
-#         return out
+class GPT(nn.Module):
+    """  the full GPT language model, with a context size of block_size """
+
+    def __init__(self, d_model, h=8, block_exp=4,
+                 n_layer=8, vert_anchors=8, horz_anchors=8,
+                 embd_pdrop=0.1, attn_pdrop=0.1, resid_pdrop=0.1):
+        super().__init__()
+
+        self.n_embd = d_model
+        self.vert_anchors = vert_anchors
+        self.horz_anchors = horz_anchors
+
+        d_k = d_model
+        d_v = d_model
+
+        # positional embedding parameter (learnable), rgb_fea + ir_fea
+        self.pos_emb = nn.Parameter(torch.zeros(1, 2 * vert_anchors * horz_anchors, self.n_embd))
+
+        # transformer
+        self.trans_blocks = nn.Sequential(*[myTransformerBlock(d_model, d_k, d_v, h, block_exp, attn_pdrop, resid_pdrop)
+                                            for layer in range(n_layer)])
+
+        # decoder head
+        self.ln_f = nn.LayerNorm(self.n_embd)
+
+        # regularization
+        self.drop = nn.Dropout(embd_pdrop)
+
+        # avgpool
+        self.avgpool = nn.AdaptiveAvgPool2d((self.vert_anchors, self.horz_anchors))
+
+        # init weights
+        self.apply(self._init_weights)
+
+    @staticmethod
+    def _init_weights(module):
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=0.02)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+
+    def forward(self, x):
+        """
+        Args:
+            x (tuple?)
+
+        """
+        rgb_fea = x[0]  # rgb_fea (tensor): dim:(B, C, H, W)
+        ir_fea = x[1]   # ir_fea (tensor): dim:(B, C, H, W)
+        assert rgb_fea.shape[0] == ir_fea.shape[0]
+        bs, c, h, w = rgb_fea.shape
+
+        # -------------------------------------------------------------------------
+        # AvgPooling
+        # -------------------------------------------------------------------------
+        # AvgPooling for reduce the dimension due to expensive computation
+        rgb_fea = self.avgpool(rgb_fea)
+        ir_fea = self.avgpool(ir_fea)
+
+        # -------------------------------------------------------------------------
+        # Transformer
+        # -------------------------------------------------------------------------
+        # pad token embeddings along number of tokens dimension
+        rgb_fea_flat = rgb_fea.view(bs, c, -1)  # flatten the feature
+        ir_fea_flat = ir_fea.view(bs, c, -1)  # flatten the feature
+        token_embeddings = torch.cat([rgb_fea_flat, ir_fea_flat], dim=2)  # concat
+        token_embeddings = token_embeddings.permute(0, 2, 1).contiguous()  # dim:(B, 2*H*W, C)
+
+        # transformer
+        x = self.drop(self.pos_emb + token_embeddings)  # sum positional embedding and token    dim:(B, 2*H*W, C)
+        x = self.trans_blocks(x)  # dim:(B, 2*H*W, C)
+
+        # decoder head
+        x = self.ln_f(x)  # dim:(B, 2*H*W, C)
+        x = x.view(bs, 2, self.vert_anchors, self.horz_anchors, self.n_embd)
+        x = x.permute(0, 1, 4, 2, 3)  # dim:(B, 2, C, H, W)
+
+        # 这样截取的方式, 是否采用映射的方式更加合理？
+        rgb_fea_out = x[:, 0, :, :, :].contiguous().view(bs, self.n_embd, self.vert_anchors, self.horz_anchors)
+        ir_fea_out = x[:, 1, :, :, :].contiguous().view(bs, self.n_embd, self.vert_anchors, self.horz_anchors)
+
+        # -------------------------------------------------------------------------
+        # Interpolate (or Upsample)
+        # -------------------------------------------------------------------------
+        rgb_fea_out = F.interpolate(rgb_fea_out, size=([h, w]), mode='bilinear')
+        ir_fea_out = F.interpolate(ir_fea_out, size=([h, w]), mode='bilinear')
+
+        return rgb_fea_out, ir_fea_out
+
+
+
 
 
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
